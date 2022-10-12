174971efb504f82b37654dd5ea13e83fc3c09fd7
SIVASANKAR-JAYAVEL <113333564+sivasankar-jayavel@users.noreply.github.com>
2022-10-12 12:39:31 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,10 @@
+
+function sai(){
+    var firstname=document.getElementById("First name").value;
+    var Middlename=document.getElementById("Middle name").value;
+    var lastname=document.getElementById("Last name").value;
+    var email=document.getElementById("email").value;
+    var password=document.getElementById("pass").value;
+    console.log(firstname,Middlename,lastname,email,password);
+}
+
